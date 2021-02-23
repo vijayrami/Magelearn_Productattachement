0aca4cf258cefdb2df88561cf3b393feca067bff
Vijay Rami <vijaymrami@gmail.com>
2021-02-23 08:14:13 +0530

    Update Uninstall.php

--- Setup/Uninstall.php
+++ Setup/Uninstall.php
@@ -7,6 +7,17 @@ use Magento\Framework\Setup\ModuleContextInterface;
 
 class Uninstall implements UninstallInterface
 {
+	/**
+     	* @var EavSetupFactory
+    	 */
+    	protected $_eavSetupFactory;
+
+    	public function __construct(
+		EavSetupFactory $eavSetupFactory
+    	) {
+		$this->_eavSetupFactory = $eavSetupFactory;
+    	}
+	
 	public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
 	{
 		$this->moduleDataSetup->getConnection()->startSetup();
